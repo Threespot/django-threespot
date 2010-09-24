d4867c59b13bd59167cf48cee3c7be79fffa3cfd
James Stevenson <james.m.stevenson@gmail.com>
2010-09-24 17:39:53 -0400

    Changed argument to get_folder_path.

--- threespot/configure/__init__.py
+++ threespot/configure/__init__.py
@@ -7,11 +7,9 @@ from django.core.exceptions import ImproperlyConfigured
 Utilities for working with Django settings.
 """
 
-def get_folder_path(module, dirname):
+def get_folder_path(module_location, dirname):
     """ Return the full path to the ``dirname`` in module directory."""
-    
-    
-    here = path.dirname(module.__file__)
+    here = path.dirname(module_location)
     return path.normpath(path.join(here, dirname))
 
 class SettingsManager(object):
