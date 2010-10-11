d472db956cc6a94cef74ebc8cc3333bbb3f5ded7
James Stevenson <james.m.stevenson@gmail.com>
2010-10-11 15:41:56 -0400

    Adds richtext module

---
+++ threespot/richtext/palettes.py
@@ -0,0 +1,17 @@
+reduced_pallete = """['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', '-','Source'],
+['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+['Bold','Italic','Underline', '-', 'Link','Unlink','Anchor']"""
+
+full_pallete = """['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker'],
+['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+['Bold','Italic','Underline','-','Subscript','Superscript'],
+['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+['Link','Unlink','Anchor','-','Styles','Format'],
+['Maximize', 'ShowBlocks', 'Source','-','Image','Flash','Table','Rule','SpecialChar','PageBreak']"""
+
+list_pallete = """['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker'],
+['Undo','Redo','-','SelectAll','RemoveFormat'],['NumberedList','BulletedList'],
+['Bold','Italic','Underline', '-', 'Link','Unlink','Anchor'],['ShowBlocks', 'Source']"""
+
+tiny_pallete = """['Bold','Italic','Underline', '-', 'Link','Unlink','Anchor'],['ShowBlocks', 'Source']"""
---
+++ threespot/richtext/widget.py
@@ -0,0 +1,67 @@
+from django import forms
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+from django.utils.translation import get_language
+
+from palettes import full_pallete
+
+CKEDITOR_PATH = getattr(settings, 'THREESPOT_CKEDITOR_PATH', '') 
+
+class CKEditor(forms.Textarea):
+    """A richtext editor widget that uses CKEditor.
+    Inspired by http://code.google.com/p/django-ck
+    """
+    additional_plugins_js = ''
+    
+    class Media:
+        js = (
+            CKEDITOR_PATH + 'ckeditor.js',
+            CKEDITOR_PATH + 'adapters/jquery.js',
+        )
+        css = {
+        }
+
+    def __init__(self, *args, **kwargs):
+        
+        self.ck_attrs = kwargs.get('ck_attrs', {})
+        if self.ck_attrs:
+            kwargs.pop('ck_attrs')
+        
+        self.additional_plugins_js = kwargs.get('additional_plugins_js', '')
+        if self.additional_plugins_js:
+            kwargs.pop('additional_plugins_js')
+
+        self.pallete = kwargs.get('custom_pallete', full_pallete)
+        if kwargs.get('custom_pallete'):
+            kwargs.pop('custom_pallete')
+        
+        super(CKEditor, self).__init__(*args, **kwargs)
+        
+    def _serialize_script_params(self):
+        ck_attrs = ''
+        if not 'language' in self.ck_attrs:
+            self.ck_attrs['language'] = get_language()[:2]
+        dict_items = []
+        for k,v in self.ck_attrs.iteritems():
+            dict_items.append(k + " : '" + v + "'")
+        ck_attrs += ",\n".join(dict_items)
+        return ck_attrs
+    
+    def render(self, name, value, attrs=None):
+        template = """%(field)s
+        <script type="text/javascript">
+            var %(field_variable_name)s_CKeditor = new CKEDITOR.replace('id_%(field_name)s', {
+                %(options)s,
+                toolbar : [
+                    %(pallete)s,
+                    %(additional_plugins_js)
+                ]
+        </script>"""    
+        return template % {
+            'additional_plugins_js': mark_safe(self.additional_plugins_js)
+            'field': super(CKEditor, self).render(name, value, attrs),
+            'field_name': name,
+            'field_variable_name': name.replace("-", "_"),
+            'options': mark_safe(self._serialize_script_params()),
+            'pallete': mark_safe(self.pallete),
+        }
