9d4cbc273012dc5f3168c3d295bd8756fe6032e2
James Stevenson <james.m.stevenson@gmail.com>
2011-03-04 09:44:13 -0500

    Fixes bug in workflow manager

--- threespot/workflow/managers.py
+++ threespot/workflow/managers.py
@@ -9,18 +9,18 @@ class WorkflowManager(models.Manager):
     
     def published(self):
         """ Returns all published items."""
-        return super(WorkflowManager, self).get_query_set().filter(
+        return self.get_query_set().filter(
             status=PUBLISHED_STATE
         )
     
     def unpublished(self):
         """ Returns all unpublished objects."""
-        return super(WorkflowManager, self).get_query_set().exclude(
+        return self.get_query_set().exclude(
             status=PUBLISHED_STATE
         )        
     
     def draft_copies(self):
         """ Returns all draft copies."""
-        return super(WorkflowManager, self).get_query_set().exclude(
+        return self.get_query_set().exclude(
             copy_of__exact=None
         )
