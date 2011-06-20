bebd0e95da4896fac927e89631908b45ae13d6ee
James Stevenson <james.m.stevenson@gmail.com>
2011-06-20 15:49:27 -0400

    Adds a ``get_generic_referencing_models`` function, fixes some problems with ``get_referencing_models``.

--- threespot/orm/introspect.py
+++ threespot/orm/introspect.py
@@ -1,6 +1,17 @@
+from django.contrib.contenttypes import generic
 from django.db import models
 
 from threespot.functional import partial
+
+def get_generic_referencing_models(my_model):
+    model_list = []
+    for model in models.get_models():
+        for field in model._meta.fields:
+            if isinstance(field, generic.GenericForeignKey):
+                if field.rel and field.rel.to == my_model:
+                    model_list.append(model)
+                    continue
+    return model_list    
 
 def get_referencing_models(my_model, field_instance=None):
     """
@@ -17,13 +28,16 @@ def get_referencing_models(my_model, field_instance=None):
         for field in model._meta.fields:
             if field_instance:
                 if isinstance(field, field_instance):
-                    if field.rel.to == my_model:
+                    if field.rel and field.rel.to == my_model:
                         model_list.append(model)
                         continue
             else:
-                if field.rel.to == my_model:
-                    model_list.append(model)
-                    continue
+                try:
+                    if field.rel and field.rel.to == my_model:
+                        model_list.append(model)
+                        continue
+                except AttributeError:
+                    import pdb; pdb.set_trace()
     return model_list 
 
 # A shortcut function to return all models that have a 
