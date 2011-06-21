3cf99cb4873ac7620f7811b22b2707cbb5e16de9
James Stevenson <james.m.stevenson@gmail.com>
2011-06-21 10:00:35 -0400

    Cuts execution time of get_referencing_objects by a third, adds lookup_referencing_object_relationships function

--- threespot/orm/introspect.py
+++ threespot/orm/introspect.py
@@ -1,5 +1,9 @@
+from itertools import chain
+from operator import or_
+
 from django.contrib.contenttypes import generic
 from django.db import models
+from django.db.models import Q
 
 from threespot.functional import partial
 
@@ -10,23 +14,23 @@ This module provides helper functions for finding our more information about a s
 
 def get_referencing_models(my_model, field_instance=None):
     """
-    This function returns a list of all models which have some sort of relation 
-    to the given ``my_model`` model. You can restrict the types of relations by 
-    passing a related field (e.g. ``models.ManyToManyField``) to the 
-    ``field_instance`` argument. Only models which are related to your model in 
+    This function returns a list of all models which have some sort of relation
+    to the given ``my_model`` model. You can restrict the types of relations by
+    passing a related field (e.g. ``models.ManyToManyField``) to the
+    ``field_instance`` argument. Only models which are related to your model in
     that particular way will be returned.
-    
+
     Note: This function does not take Generic relations into account.
-    
+
     This function returns a dictionary:
-    
+
         {
             'model': <The referencing model>,
             'field_names': [<List of field names referencing the given model>]
             'm2m_field_names': [<List of referencing M2M field names>]
 
         }
-    
+
     """
     model_data_list = []
     for model in models.get_models():
@@ -47,23 +51,23 @@ def get_referencing_models(my_model, field_instance=None):
         if len(model_data['field_names']) > 0 \
             or len(model_data['m2m_field_names']) > 0:
             model_data_list.append(model_data)
-    return model_data_list 
+    return model_data_list
 
-# A shortcut function to return all models that have a 
+# A shortcut function to return all models that have a
 # OneToOneField referencing your model.
 get_referencing_o2o_models = partial(
     get_referencing_models,
     field_instance=models.OneToOneField
 )
 
-# A shortcut function to return all models that have a 
+# A shortcut function to return all models that have a
 # ManyToManyField referencing your model.
 get_referencing_m2m_models = partial(
     get_referencing_models,
     field_instance=models.ManyToManyField
 )
 
-# A shortcut function to return all models that have a 
+# A shortcut function to return all models that have a
 # ForeignKey referencing your model.
 get_referencing_fk_models = partial(
     get_referencing_models,
@@ -72,16 +76,15 @@ get_referencing_fk_models = partial(
 
 _default_queryset = lambda model: model.objects.all()
 
-
-def get_generic_referencing_objects(my_object, 
+def get_generic_referencing_objects(my_object,
                                     get_queryset=_default_queryset):
     """
-    This function returns a list of all objects which have some sort of generic 
+    This function returns a list of all objects which have some sort of generic
     relationship to the given ``my_object`` object. One optional argument:
-    
-        :get_queryset: A function which returns a queryset of related model 
+
+        :get_queryset: A function which returns a queryset of related model
          objects. By default, Model.objects.all() is used.
-    
+
     """
     object_list = []
     my_model = my_object.__class__
@@ -105,22 +108,38 @@ def get_generic_referencing_objects(my_object,
 
 def get_referencing_objects(my_object, get_queryset=_default_queryset):
     """
-    This function returns a list of all objects which have some sort of 
+    This function returns a list of all objects which have some sort of
     relationship to the given ``my_object`` object. One optional argument:
-    
+
         :get_queryset: A function which returns a queryset of the related model
          objects. By default, Model.objects.all() is used.
-    
+
     """
+    querysets = []
     my_model = my_object.__class__
-    referencing_objects = []
+    # For each related model, filter based on FK rel to ``my_model``...
     for model_data in get_referencing_models(my_model):
-        for field_name in model_data['field_names']:
-            for obj in get_queryset(model_data['model']):
-                if getattr(obj, field_name).pk == my_object.pk:
-                    referencing_objects.append(obj)
-        for field in model_data['m2m_field_names']:
-            for obj in get_queryset(model_data['model']):
-                if my_object.pk in [m2.pk for m2 in getattr(obj, field).all()]:
-                    referencing_objects.append(obj)
-    return referencing_objects
+        queryset = get_queryset(model_data['model'])
+        fields = model_data['field_names'] + model_data['m2m_field_names']
+        filter_kwarg_names = ("%s__pk" % f for f in  fields)
+        q_obj_list = (Q(**{kw: my_object.pk}) for kw in filter_kwarg_names)
+        querysets.append(queryset.filter(reduce(or_, q_obj_list)))
+    # ... then chain querysets together.
+    return list(chain(*querysets))
+
+def lookup_referencing_object_relationships(my_obj, referencing_obj):
+    """
+    Return a list of field names on the given ``referencing_object`` which
+    reference ``my_object``.
+    """
+    for data in get_referencing_models(my_obj.__class__):
+        fields = []
+        if data['model'] == referencing_obj.__class__:
+            for field in data['field_names']:
+                if getattr(referencing_obj, field).pk == my_obj.pk:
+                    fields.append(field)
+            for field in data['m2m_field_names']:
+                pks = [o.pk for o in getattr(referencing_obj, field).all()]
+                if my_obj.pk in pks:
+                    fields.append(field)
+            return fields
