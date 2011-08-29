309e901548876573dbae0a28f43fa82f3eafdb51
James Stevenson <james.stevenson@threespot.com>
2011-08-29 16:31:12 -0400

    Adds a little more functionality to the doc server.

--- threespot/documentation/views.py
+++ threespot/documentation/views.py
@@ -1,10 +1,15 @@
+import re
+
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.static import serve 
 
 from threespot.documentation.app_settings import PUBLISH_PATH
 
+
+_web_file = re.compile("\.css|js|html|png|gif|jpg|jpeg|ico{1}$")
+
 @staff_member_required
-def documentation(*args, **kwargs):
+def documentation(request, path, *args, **kwargs):
     """
     Uses the ugly, but good-enough django static files server. Ensures the
     server serves from the ``DOCUMENTATION_PUBLISH_PATH`` setting, and
@@ -12,4 +17,7 @@ def documentation(*args, **kwargs):
     """
     kwargs['document_root'] = PUBLISH_PATH
     kwargs['show_indexes'] = False
-    return serve(*args, **kwargs)
+    if not _web_file.search(path):
+        path += "index.html"
+    print path
+    return serve(request, path, *args, **kwargs)
