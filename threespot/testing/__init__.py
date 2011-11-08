5b37e74af45305dbc48e4f5cbad98b4b550b09b8
James Stevenson <james.stevenson@threespot.com>
2011-11-08 11:49:54 -0500

    Fixes indentation error.

--- threespot/testing/__init__.py
+++ threespot/testing/__init__.py
@@ -9,23 +9,23 @@ class TestCasePlus(TestCase):
     """
 
 
-def assert_object_in_context(self, url, context_name, obj):
-    """
-    A test case to determine if the object that exists with the given
-    ``context_name`` at the specified URL is the same as ``obj``.
-    """
-    result = self.client.get(url)
-    # Verify the URL comes back with a status code of 200.
-    self.assertEqual(result.status_code, 200)
-    # Verify the context has the value.
-    self.assertTrue(context_name in result.context)
-    context_object = result.context[context_name]
-    # Verify object and context object are instances of the same class.
-    self.assertTrue(context_object.__class__, obj.__class__)
-    # If the objects are instances of a Django model, verify they have the
-    # same primary key.
-    if issubclass(obj.__class__, models.Model):
-        self.assertEqual(context_object.pk, obj.pk)
+    def assert_object_in_context(self, url, context_name, obj):
+        """
+        A test case to determine if the object that exists with the given
+        ``context_name`` at the specified URL is the same as ``obj``.
+        """
+        result = self.client.get(url)
+        # Verify the URL comes back with a status code of 200.
+        self.assertEqual(result.status_code, 200)
+        # Verify the context has the value.
+        self.assertTrue(context_name in result.context)
+        context_object = result.context[context_name]
+        # Verify object and context object are instances of the same class.
+        self.assertTrue(context_object.__class__, obj.__class__)
+        # If the objects are instances of a Django model, verify they have the
+        # same primary key.
+        if issubclass(obj.__class__, models.Model):
+            self.assertEqual(context_object.pk, obj.pk)
 
     def verify_status_code_response(self, url, code):
         """
