b74bb18ead1bcdb86333ba6502417d8104c2ea76
James Stevenson <james.stevenson@threespot.com>
2012-08-08 16:24:22 -0400

    Reolves Issue #2: safe_email regex cannot handle emails with subdomains.

--- threespot/text/templatetags/us_localflavor.py
+++ threespot/text/templatetags/us_localflavor.py
@@ -2,12 +2,12 @@ import re
 
 from django import template
 from django.utils.encoding import force_unicode
+from django.utils.html import simple_email_re
 from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 _phone_num = re.compile("^(\d{3})(\d{3})(\d{4})(.*)")
-_email = re.compile("([\w\-][\w\-\.]+)@([\w\-]+)\.([a-zA-Z]{2,4})")
 
 def phonenumber(value):
     """
@@ -34,7 +34,7 @@ def safe_email(value):
     </script>
     """
     orig = force_unicode(value)
-    return mark_safe(_email.sub(
+    return mark_safe(simple_email_re.sub(
         "<span class=\"safe-email\">\g<1> at \g<2> dot \g<3></span>",
         orig
     ))
